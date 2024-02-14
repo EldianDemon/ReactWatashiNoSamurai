e73bfe94b0c0bfafcd70c664da4348a519ada0fb
EldianDemon <IvanIzmaylove@gmail.com>
2024-02-14 10:00:59 +0300

    back to props

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import App from './App';
 import store from './redux/redux_store';
 import { Provider } from 'react-redux';
 
-let renderEntireTree = () => {
+let renderEntireTree = (state) => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      
+        <App state = {state} dispatch = {store.dispatch} store = {store}/>
+      
     </React.StrictMode>
   );
 }
