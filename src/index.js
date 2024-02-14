d6e29283fa53c252b670e6c18a462ac097e0d6ab
EldianDemon <IvanIzmaylove@gmail.com>
2024-02-14 09:57:30 +0300

    minor changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './redux/redux_store';
+import { Provider } from 'react-redux';
 
-let renderEntireTree = (state) => {
+let renderEntireTree = () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-      <React.StrictMode>
-        <App state={state} dispatch={store.dispatch.bind(store)} store={store}/>
-      </React.StrictMode>
-    );
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
 }
 
 renderEntireTree(store.getState())
